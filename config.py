c148d48ba13c1171693e38c65c9293713d3ae446
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2024-12-27 20:07:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://t.me/jiosaavnsupport",
+    "https://github.com/DarkAarush/LU",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
